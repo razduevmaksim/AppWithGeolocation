5325c28c1e0379174fd722a8e19800d6f587c8e1
Maksim Razduev <mrazduev@gmail.com>
2022-08-01 17:59:31 +0300

    AppWithGeolocation

--- app/src/main/java/com/example/geolocation/ui/myLocationListener/MyLocationListener.kt
+++ app/src/main/java/com/example/geolocation/ui/myLocationListener/MyLocationListener.kt
@@ -1,25 +1,13 @@
-@file:Suppress("DEPRECATION")
-
 package com.example.geolocation.ui.myLocationListener
 
 import android.location.Location
 import android.location.LocationListener
-import android.os.Bundle
 
 class MyLocationListener : LocationListener {
     private lateinit var myLocationListenerInterface: MyLocationListenerInterface
 
     override fun onLocationChanged(location: Location) {
         myLocationListenerInterface.myOnLocationChanged(location)
-    }
-
-    @Deprecated("Deprecated in Java", ReplaceWith(
-        "super.onStatusChanged(provider, status, extras)",
-        "android.location.LocationListener"
-    )
-    )
-    override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {
-        super.onStatusChanged(provider, status, extras)
     }
 
     fun setMyLocationListenerInterface(myLocationListenerInterface: MyLocationListenerInterface){
